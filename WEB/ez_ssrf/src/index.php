9510facd5cd6676712c85741f323c29db7f4f161
Abyssun <2025353414@qq.com>
2023-03-14 21:25:40 +0800

    upload

---
+++ WEB/ez_ssrf/src/index.php
@@ -0,0 +1,21 @@
+<?php
+
+highlight_file(__FILE__);
+error_reporting(0);
+
+$data=base64_decode($_GET['data']);
+$host=$_GET['host'];
+$port=$_GET['port'];
+
+$fp=fsockopen($host,intval($port),$error,$errstr,30);
+if(!$fp) {
+    die();
+}
+else {
+    fwrite($fp,$data);
+    while(!feof($data))
+    {
+        echo fgets($fp,128);
+    }
+    fclose($fp);
+}
