ea85d85da84c6f3c39fc651ffb6b7e5e6b3a2ffe
NewBoy06 <102408604+NewBoy06@users.noreply.github.com>
2023-03-16 14:39:16 +0800

    upload

---
+++ WEB/cookie/src/index.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+header("Content-Type:text/html;charset=utf-8");
+highlight_file(__FILE__);
+if($_COOKIE['admin']==1)
+{
+    include "../next.php";
+}
+else
+    echo "小饼干最好吃啦！";
+?> 小饼干最好吃啦！<?php
